adda3fb193e7a61e77a0f96ff937d6b8c4696991
Petr Michal <petrmichal0@gmail.com>
2024-08-11 19:49:02 +0200

    ADD StackNavigator

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,12 +1,21 @@
 import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
